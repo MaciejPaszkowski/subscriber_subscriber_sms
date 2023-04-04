04e0743bcc6c6cce31c4ddee7dcb4e9571f21f4a
maciej-paszkowski-gt <maciej.paszkowski.gt@gmail.com>
2023-04-04 10:13:03 +0200

    adding models and django structures

---
+++ subscriber_core/api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Subscriber(models.Model):
+    id = models.IntegerField(primary_key=True)
+    create_data = models.DateTimeField()
+    email = models.CharField(max_length=240, unique=True)
+    gdpr_consent = models.BooleanField()
+
+
+class SubscriberSMS(models.Model):
+    id = models.IntegerField(primary_key=True)
+    create_data = models.DateTimeField()
+    phone = models.CharField(max_length=20, unique=True)
+    gdpr_consent = models.BooleanField()
+
+
+class Client(models.Model):
+    id = models.IntegerField(primary_key=True)
+    create_data = models.DateTimeField()
+    email = models.CharField(max_length=240, unique=True)
+    phone = models.CharField(max_length=20)
+
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    create_data = models.DateTimeField()
+    email = models.CharField(max_length=240)
+    phone = models.CharField(max_length=20)
+    gdpr_consent = models.BooleanField()
